f2fd30f5634e440a0d18e5ef7c4de291987882f8
vishak45 <vishak.r565@gmail.com>
2026-02-05 22:23:49 +0530

    feat: add INTERESTS data and integrate into App component for user selection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import { useGeolocation } from "../hooks/locations";
-
+import { INTERESTS } from "../services/service";
 export default function Home() {
   const { location, loading, error } = useGeolocation();
+  const [interest, setInterest] = useState(null);
 
   if (loading) return <p>Getting your location...</p>;
   if (error) return <p>Error: {error}</p>;
 
   return (
     <div>
-      <h2>Your Location</h2>
+      <h2>Select Interest</h2>
+
+      {INTERESTS.map((item) => (
+        <button
+          key={item.value}
+          onClick={() => setInterest(item.value)}
+          style={{
+            margin: "5px",
+            background: interest === item.value ? "#4f46e5" : "#ddd",
+            color: interest === item.value ? "#fff" : "#000"
+          }}
+        >
+          {item.label}
+        </button>
+      ))}
+
+      {interest && (
+        <p>
+          Selected: <b>{interest}</b>
+        </p>
+      )}
+
+      <hr />
+
       <p>Latitude: {location.lat}</p>
       <p>Longitude: {location.lng}</p>
     </div>
